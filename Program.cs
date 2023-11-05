4101f5825fe434a6a7c6584385de76e6190b96e8
workenjoyer <149963035+workenjoyer@users.noreply.github.com>
2023-11-05 20:46:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Автомобиль
+{
+    private string name;
+    private int maxSpeed;
+
+    public Автомобиль(string _name)
+    {
+        name = _name;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine("Название автомобиля: " + name);
+        Console.WriteLine("Максимальная скорость: " + maxSpeed);
+    }
+
+    public void SetMaxSpeed(int speed)
+    {
+        maxSpeed = speed;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Автомобиль Машина1 = new Автомобиль("Corvette");
+        Автомобиль Машина2 = new Автомобиль("Bugatti");
+        Автомобиль Машина3 = new Автомобиль("McLaren");
+        Автомобиль Машина4 = new Автомобиль("Ferarri");
+
+
+        Машина1.SetMaxSpeed(320);
+        Машина2.SetMaxSpeed(400);
+        Машина3.SetMaxSpeed(340);
+        Машина4.SetMaxSpeed(350);
+
+        Машина1.ShowInfo();
+        Машина2.ShowInfo();
+        Машина3.ShowInfo();
+        Машина4.ShowInfo();
+
+
+    }
+}
